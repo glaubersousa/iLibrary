b7409b06a3a4d8269b51fc26af28f1c387d424f8
glaubersousa <glaubersousa45@gmail.com>
2024-08-14 16:57:44 -0300

    Inicializando todos os arquivos do projeto

---
+++ src/main/java/com/isystem/ilibraryapi/ILibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.isystem.ilibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILibraryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ILibraryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isystem/ilibraryapi/ILibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isystem.ilibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILibraryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
